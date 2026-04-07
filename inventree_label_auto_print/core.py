e48a0bd49bf4ca32b4e146c3a5318e31c428c916
gunstr <gunnar@adalo.se>
2026-04-07 18:00:42 +0000

    Working implementation with hard coded printer and label

--- inventree_label_auto_print/core.py
+++ inventree_label_auto_print/core.py
@@ -1,8 +1,20 @@
 """Triggers a print of a stock item label when a PO item is received"""
 
+from venv import logger
+
 from plugin import InvenTreePlugin
 
 from plugin.mixins import EventMixin, SettingsMixin
+
+from stock.models import StockItem
+from report.models import LabelTemplate
+import report.helpers
+
+from plugin.builtin.labels.inventree_machine import get_machine_and_driver
+from machine.machine_types import LabelPrinterBaseDriver, LabelPrinterMachine
+from plugin.machine import call_machine_function, registry
+
+from InvenTree.tasks import offload_task
 
 from . import PLUGIN_VERSION
 
@@ -38,15 +50,47 @@ class InvenTreeLabelAutoPrint(EventMixin, SettingsMixin, InvenTreePlugin):
         }
     }
 
+    QL700_UUID = "8a74d153-55f4-422f-8e86-a2d90cf78466"
+
     # Respond to InvenTree events (from EventMixin)
     # Ref: https://docs.inventree.org/en/latest/plugins/mixins/event/
     def wants_process_event(self, event: str) -> bool:
         """Return True if the plugin wants to process the given event."""
         # Example: only process the 'create part' event
-        return event == "part_part.created"
+        return event == "purchaseorderitem.received"
 
     def process_event(self, event: str, *args, **kwargs) -> None:
         """Process the provided event."""
-        print("Processing custom event:", event)
-        print("Arguments:", args)
-        print("Keyword arguments:", kwargs)
+        if event == 'purchaseorderitem.received':
+            stock_item_ids: list[str] | None = kwargs.get('item_ids')
+
+            try:
+
+                machine: LabelPrinterMachine | None = None
+                driver: LabelPrinterBaseDriver | None = None
+                machine, driver = get_machine_and_driver(self.QL700_UUID)
+
+                if machine is None or driver is None:
+                    logger.error("No valid machine or driver found for label printing.")
+                    return
+
+                # 1. Get the newly created stock item
+                item = [StockItem.objects.get(pk=stock_item_ids[0])] if stock_item_ids else []
+
+                # Get the label template you want to use
+                label = LabelTemplate.objects.get(pk=7)  # Get your label template
+
+                if label and item:
+                    offload_task(
+                        call_machine_function,
+                        machine.pk,
+                        'print_labels',
+                        label,
+                        item,
+                        printing_options={'copies': 1},
+                        group='plugin',
+                    )
+
+            except (StockItem.DoesNotExist, Exception) as e:
+                # Log the error if needed
+                print(f"AutoPrint Error: {e}")
